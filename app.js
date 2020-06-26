f15e47f74eeab2134a5b57754b7fd29c77a93192
Turetska <oturetska@yahoo.com>
2020-06-26 14:50:03 -0400

    made checkbox function to work

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+
 $(document).ready(function () {
 
+var isChecked = false
+    // assign function to onclick property of checkbox
+document.getElementById("noflight").onclick = function () {
+    // access properties using this keyword
+    console.log(this.checked);
+    isChecked = this.checked
+    
+};
     // onclick event            
     // creating click button to return the departure city
 
@@ -20,43 +29,40 @@ $(document).ready(function () {
 
         var city = $("#searchDestination").val() || "Stockholm"
 
+if (!isChecked) {  
+    
+    var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=" + city,
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        "x-rapidapi-key": "f4e7fa0f3amsh07cbec10686c47cp142668jsne2338ac4ac2e"
+    }
+}
 
 
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=" + city,
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-                "x-rapidapi-key": "f4e7fa0f3amsh07cbec10686c47cp142668jsne2338ac4ac2e"
-            }
-        }
+$.ajax(settings).done(function (response) {
+    console.log("Flight info = " + JSON.stringify(response));
 
+    var flights = "";
 
-        $.ajax(settings).done(function (response) {
-            console.log("Flight info = " + JSON.stringify(response));
+    for (let i = 0; i < 5; i++) {
+        $("#flights").html(flights);
+        flights += "<div id='flightsoptions'> Destination Airport:" + [i+1];
+        flights += "<p>" + response.Places[i].CountryName + "</p>";
+        flights += "<p>" + response.Places[i].PlaceName + "</p>";
+        flights += "<p>" + response.Places[i].PlaceId + "</p>";
+    }
 
-            var flights = "";
+});
 
-            for (let i = 0; i < 5; i++) {
-                $("#flights").html(flights);
-                flights += "<div id='flightsoptions'>";
-                flights += "<p>" + response.Places[i].CountryName + "</p>";
-                flights += "<p>" + response.Places[i].PlaceName + "</p>";
-                flights += "<p>" + response.Places[i].PlaceId + "</p>";
-            }
+}
 
-        });
+      
 
-        // assign function to onclick property of checkbox
-        document.getElementById("noflight").onclick = function () {
-            // access properties using this keyword
-            if (this.checked) {
-                // if checked ...
-                return; //stop the execution of function
-            }
-        };
+        
 
 
         // skyscanner: date to be shown, photo, descritpion, date, location
